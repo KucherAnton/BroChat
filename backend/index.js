185b70feb3137c33598604e782fdd056d8f6bcb0
KucherAnton <kapetrovi4@gmail.com>
2022-06-23 03:31:56 +0300

    adding server

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,16 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+const express = require('express')
+const mongoose = require('mongoose')
+const PORT = process.env.PORT || 5000
 
-server.listen(3000);
+const app = express()
 
-// app.get('/', function (request, response) {
-// 	response.sendFile(__dirname + '/index.html');
-// });
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb+srv://Anton_Kucher:D4xChdNU@cluster0.ydqdqqs.mongodb.net/auth_roles?retryWrites=true&w=majority')
+        app.listen(PORT, () => console.log(`server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start ()
